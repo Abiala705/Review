a4d610690eb65da5e972b7ef69c0d490beb5fce8
Abiala705 <abialaisrael1998@gmail.com>
2024-02-01 16:07:52 +0100

    first commit

---
+++ assignments.js
@@ -0,0 +1,10 @@
+let c = 'quality';
+console.log(c);
+
+let country = 'Nigeria';
+let continent = 'Africa';
+let population = '200 million';
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+let js = 'amazing';
+
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+
+let firstName = 'Matilda';
+console.log(firstName)
+firstName = 'Israel';
+
+console.log(firstName);
+
+let jonas_matilda = 'JM';
+
+let Person = 'Jonas';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+*/
