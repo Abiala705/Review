7d81e68e3709e6cde004db621bddf0cca0b9943d
Abiala705 <abialaisrael1998@gmail.com>
2024-02-03 21:59:02 +0100

    refresher saturday

--- script.js
+++ script.js
@@ -146,7 +146,7 @@ console.log(`String with
 multiple
 lines`);
 
-*/
+
 
 
 const age = 15;
@@ -167,3 +167,75 @@ if (birthYear <= 2000) {
 } else {
     let century = 21;
 }
+
+
+
+
+// Type conversion
+const inputYear = '1991';
+console.log(inputYear, Number(inputYear));
+
+console.log(Number(inputYear) + 18);
+
+console.log(Number('jonas'));
+
+console.log(typeof (NaN));
+
+console.log(String(23));
+
+
+// Type coercion
+
+console.log('I am ' + 23 + ' years old.');
+
+console.log('23' - '10' - 3);
+
+console.log('23' * '2');
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
+
+
+// Five falsy values
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+
+const money = 100;
+
+
+// Javascript tries to coerce the valuw into Boolean
+if (money) {
+    console.log("Don't spend it all;")
+} else {
+    console.log('You should get a job!')
+}
+
+let height = 0;
+
+if (height) {
+    console.log("YAY... Height is defined")
+} else {
+    console.log('Height is UNDEFINED')
+}
+
+*/
+
+// Equality variables
+
+const age = 18;
+
+if (age == '18') console.log('You just became an adult.');
+//  Avoid loose equality operator.
+
+const favourite = Number(prompt('What is your favourite number?'));
+console.log(favourite);
+console.log(typeof (favourite));
+
+if (favourite === 23) {
+    console.log('Cool! 23 is an amazing number.')
+}
+
